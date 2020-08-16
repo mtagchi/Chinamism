46e5b85ace9affa5682aa79c1d7c5069c2cdb02c
Zara Yakob <buscasnaz@gmail.com>
2020-08-17 00:19:23 +0900

    Fix header

--- header.php
+++ header.php
@@ -12,18 +12,17 @@
 
 <body <?php body_class(); ?>>
   <header>
+    <div class="nav-left">
+      <h1 class="chinami_logo">
+          <a href=<?php echo home_url( '/' ) ?>>CHINAMISM</a>
+        </h1>
+    </div>
 
-  <div class="nav-left">
-    <h1 class="chinami_logo">
-        <a>CHINAMISM</a>
-      </h1>
-  </div>
-
-  <div class="nav-right">
+    <div class="nav-right">
       <ul class="nav-list">
-        <a href="#" class="nav-list-item">Profile</a>
-        <a href="#" class="nav-list-item">Skills</a>
-        <a href="#" class="nav-list-item">Contact</a>
+        <a href=<?php echo home_url( '/#' ) ?> class="nav-list-item">Profile</a>
+        <a href=<?php echo home_url( '/#' ) ?> class="nav-list-item">Skills</a>
+        <a href=<?php echo home_url( '/#' ) ?> class="nav-list-item">Contact</a>
         <a href="schedule" class="nav-list-item">Schedule</a> 
       </ul>
     </div>
