659e3300e275f810befdde73cc1127831fbca32e
Zxy0129yxz <56748017+Zxy0129yxz@users.noreply.github.com>
2020-08-29 18:51:46 +0900

    complete top

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0 ">
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-  <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap" rel="stylesheet" type="text/css">
   <?php wp_head(); ?>
 </head>
 
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 
 <main>
-  <h1>Index</h1>
+  <section class="video-container">
+  <video src= "http://chinamism.local/wp-content/uploads/2020/08/test_video.mp4" autoplay muted>
+    </video>
+  <h1 class="top-quote">“Dance like no one's watching.<br>  Sing like no one's listening.”</h1>
+  </section>
 </main>
 
 <?php get_footer(); ?>
