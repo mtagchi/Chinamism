69aa6bd786760fb823a63030a60023061f682a2f
Zara Yakob <buscasnaz@gmail.com>
2020-09-02 21:55:04 +0900

    Complete contact

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0 ">
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
   <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap" rel="stylesheet" type="text/css">
+  <script type="text/javascript" src="/scripts/emailpage.js"></script>
   <?php wp_head(); ?>
 </head>
 
@@ -20,10 +21,10 @@
 
     <div class="nav-right">
       <ul class="nav-list">
-        <a href=<?php echo home_url( '/#profile' ) ?> class="nav-list-item">Profile</a>
-        <a href=<?php echo home_url( '/#skills' ) ?> class="nav-list-item">Skills</a>
-        <a href=<?php echo home_url( '/#experience' ) ?> class="nav-list-item">Experience</a>
-        <a href=<?php echo home_url( '/#contact' ) ?> class="nav-list-item">Contact</a>
+        <a href="/#profile" class="nav-list-item">Profile</a>
+        <a href="/#skills" class="nav-list-item">Skills</a>
+        <a href="/#experience" class="nav-list-item">Experience</a>
+        <a href="/#contact" class="nav-list-item">Contact</a>
         <a href="schedule" class="nav-list-item">Schedule</a>
       </ul>
     </div>
