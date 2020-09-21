5702450f96615c5172d0a7acc250abde39495573
Zxy0129yxz <56748017+Zxy0129yxz@users.noreply.github.com>
2020-09-21 18:46:36 +0900

    profile complete

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-jQuery(function () {
-  
-});
+// jQuery(document).ready(function(){
+//     var docWidth = jQuery('body').width(),
+//         $wrap = jQuery('#wrap'),
+//         $images = jQuery('#wrap .hb'),
+//         slidesWidth = $wrap.width();
+    
+//     jQuery(window).on('resize', function(){
+//       docWidth = jQuery('body').width();
+//       slidesWidth = $wrap.width();
+//     })
+    
+//     jQuery(document).mousemove(function(e) {
+//       var mouseX = e.pageX,
+//           offset = mouseX / docWidth * slidesWidth - mouseX / 2;
+      
+//       $images.css({
+//         '-webkit-transform': 'translate3d(' + -offset + 'px,0,0)',
+//                 'transform': 'translate3d(' + -offset + 'px,0,0)'
+//       });
+//     });
+//   })
